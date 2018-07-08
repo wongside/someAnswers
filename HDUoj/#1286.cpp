c0036fb98db48c399b6a97c6f83febb13a4d2572
Side <31005168+SideWang@users.noreply.github.com>
2018-07-08 17:18:14 +0800

    更改方法题目1286的方法
    使用欧拉函数计算与n互质的数

--- HDUoj/#1286.cpp
+++ HDUoj/#1286.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <map>
 using namespace std;
-#define LOCAL 0
+#define LOCAL 1
 
-map<int,int> m;//大的在前 
-
-int gcd(int x,int y){
-	
-	if(m[x] == y){
-		return 1;
-	}else if(m[x] == -y){
-		return 0;
-	}else{
-		int t = x % y;
-		if(t == 0){
-			if(y == 1){
-				m.insert(pair<int,int>(x,y));
-				return 1;
-			}else{
-				m.insert(pair<int,int>(x,-y));
-				return 0;
-			}
-		}			
-		return gcd(y,t);		
+//返回小于n且与n互质的数的个数 
+int euler(int n){ 
+	int ans = n;
+	int tmp = n;
+ 	for(int i = 2; i*i <= tmp; i++){
+		if(tmp%i == 0){
+			ans = ans / i * (i - 1); 
+			while(tmp % i == 0){
+				tmp /= i;			 
+			} 
+		}
 	}
-	
+	if(tmp>1) {
+		ans = ans / tmp * (tmp-1);
+	}
+	return ans;
 }
-
 
 
 int main(){   
@@ -38,13 +31,9 @@ int main(){
 	int n;
 	cin >> n;
 	for(int i = 0;i < n;i++){
-		int tmp,count = 0;
-		cin >> tmp;
-		for(int j = 1;j < tmp;j++){
-			if(gcd(tmp,j) == 1)
-				count++;
-		}
-		cout << count << endl;
+		int t;
+		cin >> t;
+		cout << euler(t) << endl;
 	}
 	
 	
