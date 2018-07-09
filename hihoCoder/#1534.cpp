4f3ff3aeef2edf77766fd22d5522e86535df0a02
wongside <>
2018-07-10 00:56:34 +0800

    增加了代码的注释

--- hihoCoder/#1534.cpp
+++ hihoCoder/#1534.cpp
@@ -5,8 +5,8 @@ using namespace std;
 #define LOCAL 0
 
 map<long long, int> m;
-long long s[100010];
-int tab[100010];
+long long s[100010];	//存储前缀和 
+int tab[100010];		//存储数组 
 
 
 int main(){ 
@@ -19,18 +19,18 @@ int main(){
 	cin >> n;
 	for(int i = 1;i <= n; i++){
 		cin >> tab[i];
-		s[i] = s[i-1] + tab[i];	
+		s[i] = s[i-1] + tab[i];	// 计算前缀和 
 		if(i < n)
-			m[s[i]]++;		
+			m[s[i]]++;			// 统计前缀和相同的个数 
 	}
 	long long s3 = 0;
-	for(int q = n; q > 2;q--){
-		s3 += tab[q];
-		m[s[q-1]]--;
+	for(int q = n; q > 2;q--){	//枚举最后一段数组的值 
+		s3 += tab[q];			//最后一段数组的和 
+		m[s[q-1]]--;			//前缀和的相同的数量减少 
 		for(long long s1 = s3-1;s1 <= s3 + 1;s1++){
-			long long s2 = s[n] - s1 - s3;
+			long long s2 = s[n] - s1 - s3;//计算中间一段数组的值 
 			if(abs(s2 - s1) <= 1 && abs(s3 - s1) <= 1 && abs(s2 - s3) <= 1){
-				ans += m[s1];
+				ans += m[s1];//满足条件则累加结果 
 			}
 		}
 	}
