ada9e52479bb51949af24ebfb838f7c3bec2dcff
Side <wongside@163.com>
2018-09-20 23:02:31 +0800

    增加了一道题解

--- HDUoj/#1717.cpp
+++ HDUoj/#1717.cpp
@@ -1,29 +1,65 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
-int main(){
+//求最大公约数 
+int gcd(int a, int b){			//传人参数，a > b 
+	int tmp = a % b;			//计算a模b的值 
+	if(tmp == 0){				//若为0
+		return b;				//返回b 
+	}
+	return gcd (b, tmp);		//递归调用 
+}
+
+int main(){ 
 	string a, b, s;
-	int n, len, i;
+	int n, tmp, x, y;
 	cin >> n;
 	while(n--){
 		cin >> s;
-		
-		a = s.substr() s.find('(');
-		
-		
-		len = s.length();
-		a = -1;
-		b = -1;
-		for(i = 2; i < len && s[i] != '('; i++){
-			a *= 10;
-			a += s[i] - '0';
+		x = 0;
+		y = 0;
+		a = "";
+		b = "";
+		tmp = s.find('(');				//以（来分离循环部分和不循环部分 
+		if(tmp != -1){					//找到（，为循环小数 
+			b = s.substr(tmp+1);		//分离出循环部分 
+			b.erase(b.length()-1);		//去掉最后的）符号 
+//			cout << s.substr(tmp+1, s.length())  << endl;
+//			cout << s.substr(tmp+1, s.length()-1) << endl;
+//			cout << s.substr(tmp+1, s.length()-2) << endl;
+			if(tmp != 2){				//若数有不循环部分 
+				a = s.substr(2, tmp-2);	//分离出不循环部分 
+			}			
+		}else{
+			a = s.substr(2);			//分离出不循环部分 
 		}
-		for(i++; i < len && s[i] != ')'; i++){
-			b *= 10;
-			b += s[i] - '0';
+		//将字符串转换为数字 
+		for(unsigned int i = 0; i < a.length(); i++){
+			x *= 10;
+			x += (a[i] - '0');
 		}
-		cout << a << b << endl;
+		for(unsigned int i = 0; i < b.length(); i++){
+			y *= 10;
+			y += (b[i] - '0');
+		}
+		
+		//小数转换为分数 
+		if(b.length() == 0){							//不循环小数情况 
+			int t1 = pow(10, a.length());				
+			int t2 = gcd(x, t1);
+			cout << x / t2 << '/';
+			cout << t1 / t2 << endl;
+		}else{
+			int t1 = x * pow(10, b.length()) + y - x;
+			int t2 = pow(10, a.length() + b.length()) - pow(10, a.length());
+			int t3 = gcd(t1, t2);
+			cout << t1 / t3 << '/';
+			cout << t2 / t3 << endl;
+		}
+		
+
 	}
 	
 	
