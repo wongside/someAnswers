6eb9658c5b48fe61005c0265424a72b55aed0749
Side <wongside@163.com>
2019-03-04 19:06:24 +0800

    TLE
    所有测试点都TLE

--- 洛谷/P1309.cpp
+++ 洛谷/P1309.cpp
@@ -2,13 +2,17 @@
 #include <algorithm>
 using namespace std;
 
+
 struct P{
 	int num;
 	int sco;
 	int str;
 };
 
+
 P p[200000+10];
+P t[200000+10];
+
 
 int f(P a, P b){
 	if(a.sco == b.sco){
@@ -17,35 +21,64 @@ int f(P a, P b){
 	return a.sco > b.sco;
 }
 
+int value(int i, int c){
+	if(f(p[2*i - 1],p[2*i]) == c){
+		t[i].sco = p[2*i - 1].sco;
+	}else{
+		t[i].sco = p[2*i].sco;
+	}
+}
+
+int merge(int n){
+	for(int i = 1; i <= n; i++){
+		t[i].num = p[i].num;
+		t[i].sco = value(i,1);
+	}			
+	
+	for(int i = n + 1; i <= 2*n; i++){
+		t[i].sco = value(i-n,0);		
+	}
+	
+	int lag = 1;
+	int sml = n + 1;
+	for(int i = 1; i <= 2*n;){
+		while(lag <= n && t[lag].sco > t[sml].sco){
+			p[i++] = t[lag++];
+		}
+		while(sml <= 2*n && t[sml].sco > t[lag].sco){
+			p[i++] = t[sml++];
+		}
+	}
+	
+}
+
+
 int main(){
 	int n,r,q;
 	cin >> n >> r >> q;
 	for(int i = 1; i <= 2*n; i++){
 		p[i].num = i;
-		cin >> p[i].sco; 
+		cin >> p[i].sco;
 	}
 	for(int i = 1; i <= 2*n; i++){	
 		cin >> p[i].str; 
-	}
-	
+	}	
 	
 	sort(p+1,p+2*n+1,f);
-//	for(int i = 1; i <= 2*n; i++){
-//		cout << i << " ";
-//		cout << p[i].num << " ";
-//		cout << p[i].sco << " ";
-//		cout << p[i].str << " " << endl;
-//	}
+//	cout << p[1].sco << endl;
 	for(int i = 0; i < r; i++){
-		for(int i = 1; i <= n; i++){
-			if(p[i*2].str > p[i*2-1].str){
-				p[i*2].sco++;
+		for(int j = 1; j <= n; j++){
+			if(p[j*2].str > p[j*2-1].str){
+				p[j*2].sco++;
 			}else{
-				p[i*2-1].sco++;
+				p[j*2-1].sco++;
 			}
 		}
-		sort(p+1,p+2*n+1,f);		
+		
+		merge(n);
+			
 	}
+	cout << "***" << endl;
 	cout << p[q].num;
 	
 	return 0;
