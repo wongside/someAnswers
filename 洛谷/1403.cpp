3533bf519fd69ce7f6c19cd30cbe65058b0f1868
Side <wongside@163.com>
2019-05-21 10:17:55 +0800

    Create 1403.cpp

---
+++ 洛谷/1403.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	
+	int n, ans = 0;
+	cin >> n;
+	
+	for(int i = 1; i <= n; i++){
+		ans += n/i;
+	}
+	
+	cout << ans;
+	
+	return 0;
+}
